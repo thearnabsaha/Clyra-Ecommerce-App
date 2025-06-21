b914934845c5f9219f4b5476c1f9f1329fc0225e
thearnabsaha <thearnabsaha201@gmail.com>
2025-06-21 11:19:00 +0530

    feat(graphql): add user query to schema and implement resolver

--- packages/graphql/src/resolvers.ts
+++ packages/graphql/src/resolvers.ts
@@ -2,6 +2,10 @@ import { prisma } from '@workspace/database/client'
 export const resolvers = {
   Query: {
     hello: () => 'Hello from GraphQL in TurboRepo!',
+    user: async (name:string) => {
+      const data=await prisma.user.findFirst({where:{username:name}});
+      return data;
+    },
     users: async () => {
       return await prisma.user.findMany();
     },
--- packages/graphql/src/schema.ts
+++ packages/graphql/src/schema.ts
@@ -11,5 +11,6 @@ export const typeDefs = gql`
   type Query {
     hello: String
     users: [User!]!
+    user(name:String!): User
   }
 `;
